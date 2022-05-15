06849c9d176d075a945837a33113ef89bb121c1f
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2022-05-15 02:28:52 -0300

    Fix routes redirections (use 302 instead of 301)

--- config/routes.rb
+++ config/routes.rb
@@ -16,12 +16,12 @@ Rails.application.routes.draw do
       post '/sign_up', to: 'registrations#create', as: 'registration'
     end
 
-    root to: redirect('/users/sign_in')
+    root to: redirect('/users/sign_in', status: 302)
   end
 
   constraints user_access do
     namespace :users do
-      get '/sign_in', to: redirect('/todos/uncompleted')
+      get '/sign_in', to: redirect('/todos/uncompleted', status: 302)
 
       delete '/logout', to: 'sessions#destroy'
     end
@@ -36,7 +36,7 @@ Rails.application.routes.draw do
       end
     end
 
-    root to: redirect('/todos/uncompleted'), as: :users_root
+    root to: redirect('/todos/uncompleted', status: 302), as: :users_root
   end
 
   match '/404', to: 'errors#not_found', via: :all
